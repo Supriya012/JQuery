04b522c8860332a81dc4c1fe991113d8781eb928
Supriya012 <99476296+Supriya012@users.noreply.github.com>
2022-05-19 07:19:12 +0100

    Add files via upload

---
+++ jquery-first-program/index.js
@@ -0,0 +1,37 @@
+  $("h1").text("JQuery Rocks");
+ // $("p").text(<em>"sunday"</em>);
+//$("button").html("<em> hi </em>");
+//$("img").attr("src");
+$("a").attr("href", "https://www.yahoo.com");
+$("body").css("background-color","red");
+$("h1").click(function(){
+$("h1").css("color", "green");
+
+});
+$("button.first").click(function(){
+$("h1").css("color", "purple");
+
+});
+$("button.second").click(function(){
+     $("h1").css("color", "blue");
+
+     
+     });
+$("button.third").click(function(){
+     $("h1").css("color", "yellow");
+     
+     
+     });
+     $("button.Fourth").click(function(){
+        $("h1").css("color", "Orange");
+        
+        
+        });
+        $("button.Fifth").click(function(){
+            $("h1").css("color", "Pink");
+            
+            
+            });
+            $("p").click(function(){
+                $("p").css("color","White")
+            });
